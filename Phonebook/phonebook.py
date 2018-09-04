20ddd62a0e8507a48e18bb1dbb7ec7c9a710c86a
surya1singh <surya1.singh@gmail.com>
2018-09-04 16:18:51 +0530

    add is consistent method in phonebook

--- Phonebook/phonebook.py
+++ Phonebook/phonebook.py
@@ -1,13 +1,47 @@
-
+from itertools import combinations
 class Phonebook:
     def __init__(self):
         self.entries = {}
-    
+
     def add(self, name, number):
         self.entries[name] = number
-    
+
     def lookup(self, name):
         return self.entries[name]
-        
+
     def is_consistent(self):
+        if len(self.get_numbers()) == len(set(self.get_numbers())):
+            for i,j in combinations(self.get_numbers(),2):
+                if i.startswith(j) or j.startswith(i):
+                    return False
+        else:
+            return False
+            
         return True
+
+    def get_names(self):
+        return self.entries.keys()
+
+    def get_numbers(self):
+        return self.entries.values()
+
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_with_numbers_that_prefix_one_another_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "123")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_adds_names_and_numbers(self):
+        self.phonebook.add("Sue", "12345")
+        self.assertIn("Sue", self.phonebook.get_names())
+        self.assertIn("12345", self.phonebook.get_numbers())
